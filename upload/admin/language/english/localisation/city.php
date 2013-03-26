9d81bfe7d4604e1f6d4631efbc1a2f3d4ef9429e
bogalakon <user3@server.fake>
2013-03-26 11:49:56 +0700

    new file: admin/language/english/localisation/city.php

---
+++ upload/admin/language/english/localisation/city.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']		= 'City';
+
+// Text
+$_['text_success']		= 'Success: You have modified cities!';
+
+// Column
+$_['column_name']		= 'City';
+$_['column_zone']		= 'Zone';
+$_['column_status']		= 'Status';
+$_['column_action']		= 'Action';
+
+// Entry
+$_['entry_status']		= 'City Status:';
+$_['entry_name']		= 'City Name:';
+
+// Error
+$_['error_permission']	= 'Warning: You do not have permission to modify cities!';
+$_['error_name']		= 'City Name must be between 3 and 128 characters!';
+?>
