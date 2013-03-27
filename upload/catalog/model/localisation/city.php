2c0c3265cfb5f0620cbd8f89fbe9213b6da0efcd
bogalakon <user3@server.fake>
2013-03-27 13:45:21 +0700

    Add AJAX change for city based on zone

--- upload/catalog/model/localisation/city.php
+++ upload/catalog/model/localisation/city.php
@@ -19,5 +19,19 @@ class ModelLocalisationCity extends Model {
 
 		return $city_data;
     }
+
+    public function getCitiesByZoneId($zone_id) {
+		$city_data = $this->cache->get('city.' . (int)$zone_id);
+
+		if (!$city_data) {
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "city WHERE zone_id = '" . (int)$zone_id . "' AND status = '1' ORDER BY name");
+
+			$city_data = $query->rows;
+
+			$this->cache->set('city.' . (int)$zone_id, $city_data);
+		}
+
+		return $city_data;
+	}
 }
 ?>
