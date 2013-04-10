d9c2c01c972600c886c0f70d3dd65ab45d480654
bogalakon <user3@server.fake>
2013-04-10 14:56:11 +0700

    adjust city language file

--- upload/admin/language/english/localisation/city.php
+++ upload/admin/language/english/localisation/city.php
@@ -6,7 +6,7 @@ $_['heading_title']		= 'City';
 $_['text_success']		= 'Success: You have modified cities!';
 
 // Column
-$_['column_name']		= 'City';
+$_['column_name']		= 'City Name';
 $_['column_zone']		= 'Zone';
 $_['column_status']		= 'Status';
 $_['column_action']		= 'Action';
@@ -14,7 +14,7 @@ $_['column_action']		= 'Action';
 // Entry
 $_['entry_name']		= 'City Name:';
 $_['entry_zone']		= 'Zone:';
-$_['entry_status']		= 'City Status:';
+$_['entry_status']		= 'Status:';
 
 // Error
 $_['error_permission']	= 'Warning: You do not have permission to modify cities!';
