04c42ab9e005df63daeb28e02ae965bc3c541901
bogalakon <user3@server.fake>
2013-04-10 14:06:02 +0700

    bahasa indonesia

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ocid/admin/');
+define('HTTP_CATALOG', 'http://localhost/ocid/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ocid/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ocid/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\ocid/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\ocid/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\ocid/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\ocid/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\ocid/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\ocid/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\ocid/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\ocid/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\ocid/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\ocid/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\ocid/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'J35u5Ch1215t');
+define('DB_DATABASE', 'ocid');
+define('DB_PREFIX', 'ocid_');
+?>
